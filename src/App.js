a13e4057f9e27d6a29123fbe86094fd9314de164
Kailash S <>
2021-02-27 21:03:36 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Near from "./pages/Near";
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/near">Near by</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/near">
+              <Near />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
