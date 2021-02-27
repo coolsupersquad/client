14283e5019f4debf6ac9c15d513455d136fb2eec
Ryan A <ryanaspen27@gmail.com>
2021-02-27 15:17:30 -0600

    Rudamentary Google API

--- src/pages/Near.js
+++ src/pages/Near.js
@@ -1,8 +1,17 @@
+import { google } from 'google-maps';
 
-export default function() {
-    return (
-        <div>
-            Near
-        </div>
-    )
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+  <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDgYD-yjDO9bqmrFYuOZWwDKa3uX6DSJAI&callback=initMap">
+        
+    </script>
 }
+
+export default initMap;
