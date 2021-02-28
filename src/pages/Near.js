f707b230826787fb3fcf053f0c0a8f009e5db6df
Kailash S <kailash.s.2012@gmail.com>
2021-02-28 08:51:32 +0100

    Not working code

--- src/pages/Near.js
+++ src/pages/Near.js
@@ -26,6 +26,8 @@ import
 
 import "@reach/combobox/styles.css"; 
 
+import { useState, useEffect } from 'react';
+
 require('dotenv').config();
 const superagent = require('superagent');
 
@@ -60,9 +62,123 @@ const Near = () => {
     }
   );
 
+  const [ filled, setFilled ] = useState(false)
+  const [ locations, setLocations ] = useState([
+    {
+      name: "Location 1",
+      location: { 
+        lat: 41.3954,
+        lng: 2.162 
+      },
+    },
+    {
+      name: "Location 2",
+      location: { 
+        lat: 41.3917,
+        lng: 2.1649
+      },
+    },
+    {
+      name: "Location 3",
+      location: { 
+        lat: 41.3773,
+        lng: 2.1585
+      },
+    },
+    {
+      name: "Location 4",
+      location: { 
+        lat: 41.3797,
+        lng: 2.1682
+      },
+    },
+    {
+      name: "Location 5",
+      location: { 
+        lat: 41.4055,
+        lng: 2.1915
+      },
+    }
+  ])
+  
+  
+
+  const loadLocations = () => {
+    
+      superagent
+        .get('http://35.222.213.18/api/events')
+        .end((err, res) => {
+          res.body.forEach( (el) => {
+            console.log(el)
+            
+            let addressRes = el.address + ", " + el.city + ", " + el.state // address
+            let nameRes = el.name
+            // addressRes = "2557 Thresher Circle, Plano, TX"
+            console.log(addressRes)
+            try {
+              getGeocode({ address: addressRes })
+                .then((results) => getLatLng(results[0]))
+                .then(({ lat, lng }) => {
+                  let chunk = []
+                  
+                  console.log("📍 Coordinates: ", { lat, lng });
+                  let item = {
+                      name: nameRes,
+                      // address: addressRes,
+                      location: {
+                        lat: lat, //32.9858,
+                        lng: lng //-96.7501
+                      },
+                  };
+                  // chunk.push(item)
+                  console.log("Old locations, ", locations)
+                  console.log("New locations, ", [...locations, item])
+                  setLocations([...locations, item])
+                })
+                .catch((error) => {
+                  console.log("😱 Error: ", error);
+                });
+              // const results = await getGeocode({address: addressRes});
+              // // Returns the lat and lng from the JSON object aquired from the above function
+              // const {latRes, lngRes} = await getLatLng(results[0]);  // <------
+              // console.log("Location", latRes, lngRes)
+  
+              // let item = {
+              //   name: nameRes,
+              //   // address: addressRes,
+              //   location: {
+              //     lat: latRes, //32.9858,
+              //     lng: lngRes //-96.7501
+              //   },
+              // };
+              // chunk.push(item)
+              // console.log("Adding ", item)
+              console.log(locations)
+            } catch (error) {
+              console.error(error)
+            }
+            
+          });
+  
+          
+          
+        });
+  }
+
+  useEffect( () => {
+    try {
+      // setLocations();
+
+      loadLocations()
+    } catch (err) {
+      console.error(err)
+    }
+  }, [])
+
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map; 
+    console.log("Map loaded")
   });
 
   // Prevents the map from changing unless the lat and lng change.
@@ -72,16 +188,15 @@ const Near = () => {
     mapRef.current.setZoom(14);
   }); 
 
-  superagent.get('http://35.222.213.18/api/events')
-    .end((err, res) => {
-      console.log(res)
-    });
+  
 
   if (loadError) return "Error loading the map."
   if (!isLoaded) return "Map loading."
-    return (
+
+  
+  return (
       <div>
-         <Search landOn={landOn} />
+        <Search landOn={landOn} />
         <GoogleMap 
           mapContainerStyle={mapContainerStyle} 
           zoom ={8}
@@ -89,9 +204,17 @@ const Near = () => {
           center = {center}
           onLoad = {onMapLoad}
           >
+            {
+              locations && 
+              (locations.map(item => {
+                return (
+                  <Marker key={item.name} position={item.location}/>
+                )
+              }))
+            }
         </GoogleMap>
       </div>
-    )
+  )
 
 }
 export default Near
@@ -117,10 +240,12 @@ function Search({landOn})
     <div className="search"> 
     <Combobox onSelect={async (address) => {
       try {
+        console.log("Address", address)
         const results = await getGeocode({address});
         // Returns the lat and lng from the JSON object aquired from the above function
         const {lat, lng} = await getLatLng(results[0]);  // <------
         landOn({lat, lng}); 
+        console.log({lat, lng})
       } catch(error)
       {
         console.log("error"); 
